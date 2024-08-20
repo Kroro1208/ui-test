8249d230ee10824eb97e0944d3cd06b21a05083f
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-20 21:12:58 +0900

    Control定義した

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,25 +1,37 @@
 import Button from "./Button";
+// Storybookの型定義をインポート
 import type { Meta, StoryObj } from '@storybook/react';
 
+// コンポーネントのメタデータを定義
 const meta = {
-    title: 'Button',
-    component: Button, // Buttonコンポーネント
-} as Meta<typeof Button>;
+    title: 'Button', // Storybook上でのコンポーネントの表示名
+    component: Button, // 対象のコンポーネント
+    argTypes: {
+        label: {
+            options: ["Primary Button", "Normal Button"],
+            control: { type: "select" }
+        }
+    }
+} as Meta<typeof Button>; // MetaをButtonコンポーネントの型で制約
 
+// メタデータをデフォルトエクスポート（Storybookがこれを使用）
 export default meta;
 
+// Buttonコンポーネントのストーリーの型を定義
 type Story = StoryObj<typeof Button>;
 
+// Primaryボタンのストーリーを定義
 export const Primary: Story = {
-    args: {
-        label: "Primary Button",
-        primary: true,
+    args: { // コンポーネントに渡すprops
+        label: "Primary Button", // ボタンのラベル
+        primary: true, // プライマリースタイルを適用
     }
 };
 
+// Normalボタンのストーリーを定義
 export const Normal: Story = {
-    args: {
-        label: "Normal Button",
-        primary: false,
+    args: { // コンポーネントに渡すprops
+        label: "Normal Button", // ボタンのラベル
+        primary: false, // 通常のスタイルを適用
     }
 };
